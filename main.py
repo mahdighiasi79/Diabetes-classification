02ca01c3761a002bcd26d1e79c0f039b255e7378
Mahdi Ghiasi <mmghf74@gmail.com>
2023-05-05 21:01:42 +0330

    project initialized 2

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    with open("diabetic_data.csv") as csv_file:
-        file = csv.reader(csv_file)
-        file = list(file)
-        for rows in file:
-
+    df = pd.read_csv("diabetic_data.csv")
+    print(df.iloc(0)[0])
